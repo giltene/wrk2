0dded1042ac6a01e108c7b1fb9fc95760124d28e
Janmejay Singh <singh.janmejay@gmail.com>
2016-09-25 21:51:37 +0530

    EoF on read was preventing stats collection, attempt to fix that.

--- src/net.c
+++ src/net.c
@@ -17,6 +17,7 @@ status sock_close(connection *c) {
 status sock_read(connection *c, size_t *n) {
     ssize_t r = read(c->fd, c->buf, sizeof(c->buf));
     *n = (size_t) r;
+    if (r == 0) return READ_EOF;
     return r > 0 ? OK : ERROR;
 }
 
