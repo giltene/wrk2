5bccb24ec10c016e038a3bea0ae686ad34e4dc5e
Jiri Mencak <jmencak@redhat.com>
2016-12-12 13:04:05 +0100

    Host header handling, check for no live hosts to test against

--- test/wrk.lua
+++ test/wrk.lua
@@ -1,26 +1,22 @@
--- load modules
+-- load modules -------------------------------------------------------------------------------------
 local cjson = require "cjson"
 local cjson2 = cjson.new()
 local cjson_safe = require "cjson.safe"
 
--- global variables
+-- global variables ---------------------------------------------------------------------------------
 local requests_json = "requests.json"
 local addrs   = {}
 local threads = {}	-- only for done() statistics
 local counter = 0
-local max_requests = 0
+local max_requests = 0	-- maximum request (per thread)
 local request_data
 
--- functions
-function init(args)
-  requests  = 0	-- how many requests was issued within a thread
-  responses = 0	-- how many responses was received within a thread
-
---  local msg = "thread %d init() addr: %s\n"
---  io.write(msg:format(id, wrk.thread.addr))
+-- general functions --------------------------------------------------------------------------------
+function to_integer(number)
+    return math.floor(tonumber(number) or error("Could not cast '" .. tostring(number) .. "' to number.'"))
 end
 
--- Load URL paths from the file
+-- Load URL paths from a file
 function load_request_objects_from_file(file)
   local data = {}
   local content
@@ -42,6 +38,7 @@ function load_request_objects_from_file(file)
   return data
 end
 
+-- wrk() functions ----------------------------------------------------------------------------------
 function setup(thread)
   local append = function(host, port)
     for i, addr in ipairs(wrk.lookup(host, port)) do
@@ -56,20 +53,27 @@ function setup(thread)
 
     -- Check if at least one request was found in the requests_json file
     if #requests_data <= 0 then
-      io.write("multiplerequests: No requests found.\n")
+      local msg = "setup(): no requests found in %s\n"
+      io.write(msg:format(requests_json))
       os.exit()
     end
 
 --    local msg = "setup(): host=%s; port=%d; req_data=%d\n"
     for i, req in ipairs(requests_data) do
 --      io.write(msg:format(req.host, req.port, #requests_data))
-      append(req.host, req.port)
+       append(req.host, req.port)
     end
   end
 
-  local index = counter % #addrs + 1
+  local index = (counter % #addrs) + 1 	-- we can have more connections than threads
   counter = counter + 1
   req = requests_data[index]
+
+  if req == nil then
+    io.write("setup(): no live hosts to test against\n")
+    os.exit()
+  end
+
   thread:set("id", counter)
   thread:set("method", req.method)
   thread:set("host", req.host)
@@ -83,6 +87,32 @@ function setup(thread)
 
   thread.addr = addrs[index]
   table.insert(threads, thread)
+end
+
+-- redefining wrk.init() --> for testing multiple hosts, faster than "Host" redefinitions in requests
+function wrk.init(args)
+  requests  = 0	-- how many requests was issued within a thread
+  responses = 0	-- how many responses was received within a thread
+
+  if #args >= 1 then
+--    local msg = "args=%d; args[0]=%s, args[1]=%s, args[2]=%s\n"
+--    io.write(msg:format(#args, args[0], args[1], args[2]))
+    max_requests = to_integer(args[1])
+  end
+
+--  local msg = "wrk.init(): thread %d, wrk.host=%s\n"
+--  io.write(msg:format(id, wrk.host))
+
+  wrk.headers["Host"] = host
+
+  if type(init) == "function" then
+    init(args)
+  end
+
+  local req = wrk.format()
+  wrk.request = function()
+    return req
+  end
 end
 
 -- Prints Latency based on Coordinated Omission (HdrHistogram)
@@ -110,7 +140,7 @@ function response(status, headers, body)
   responses = responses + 1
 
   -- Stop after max_requests if max_requests is a positive number
-  if (max_requests > 0) and (responses > max_requests) then
+  if (max_requests > 0) and (responses >= max_requests) then
     wrk.thread:stop()
     return
   end
