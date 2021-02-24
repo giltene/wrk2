a5e61e2a1543418e7e84a6a3c72001f60ae6b0d0
Elfranne <tom.braarup@gmail.com>
2021-02-24 19:29:29 +0100

    Work in progress

--- src/wrk.c
+++ src/wrk.c
@@ -23,6 +23,7 @@ static struct config {
     bool     record_all_responses;
     char    *host;
     char    *script;
+    char    *ciphers;
     SSL_CTX *ctx;
 } cfg;
 
@@ -57,6 +58,7 @@ static void usage() {
            "    -t, --threads     <N>  Number of threads to use   \n"
            "                                                      \n"
            "    -s, --script      <S>  Load Lua script file       \n"
+           "    -z, --ciphers     <S>  Specify SSL/TLS ciphers    \n"
            "    -H, --header      <H>  Add header to request      \n"
            "    -L  --latency          Print latency statistics   \n"
            "    -U  --u_latency        Print uncorrected latency statistics\n"
@@ -94,6 +96,15 @@ int main(int argc, char **argv) {
             ERR_print_errors_fp(stderr);
             exit(1);
         }
+
+        if (NULL != cfg.ciphers) {
+            if (ssl_set_cipher_list(cfg.ctx, cfg.ciphers) == ERROR) {
+                fprintf(stderr, "unable to set SSL ciphers: %s\n", cfg.ciphers);
+                ERR_print_errors_fp(stderr);
+                exit(1);
+            }
+        }
+
         sock.connect  = ssl_connect;
         sock.close    = ssl_close;
         sock.read     = ssl_read;
@@ -696,6 +707,7 @@ static struct option longopts[] = {
     { "duration",       required_argument, NULL, 'd' },
     { "threads",        required_argument, NULL, 't' },
     { "script",         required_argument, NULL, 's' },
+    { "ciphers",        required_argument, NULL, 's' },
     { "header",         required_argument, NULL, 'H' },
     { "latency",        no_argument,       NULL, 'L' },
     { "u_latency",      no_argument,       NULL, 'U' },
@@ -718,7 +730,7 @@ static int parse_args(struct config *cfg, char **url, struct http_parser_url *pa
     cfg->rate        = 0;
     cfg->record_all_responses = true;
 
-    while ((c = getopt_long(argc, argv, "t:c:d:s:H:T:R:LUBrv?", longopts, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "t:c:d:s:z:H:T:R:LUBrv?", longopts, NULL)) != -1) {
         switch (c) {
             case 't':
                 if (scan_metric(optarg, &cfg->threads)) return -1;
@@ -731,6 +743,9 @@ static int parse_args(struct config *cfg, char **url, struct http_parser_url *pa
                 break;
             case 's':
                 cfg->script = optarg;
+                break;
+            case 'z':
+                cfg->ciphers = optarg;
                 break;
             case 'H':
                 *header++ = optarg;
